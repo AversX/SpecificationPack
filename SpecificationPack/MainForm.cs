b954689d24ef6e71083421bd699952af847b065a
AversX <xemul.93@mail.ru>
2017-05-19 17:28:29 +0500

    Правки

--- SpecificationPack/MainForm.cs
+++ SpecificationPack/MainForm.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.IO;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.Runtime.InteropServices;
 
 namespace SpecificationPack
 {
